03c8944268048de313b2e3a2102e6e9c64d186f9
Bastien WEBER <weber.bastien@gmail.com>
2022-04-13 16:17:54 +0200

    chore: add eslint and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    plugins: ['prettier'],
+    rules: {
+        'vue/require-default-prop': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': 'warn',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+                '': 'never',
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+            },
+        },
+    },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,3 @@
-const hello = () => {console.log('hello world')};
+const hello = () => {
+    console.log('hello world');
+};
