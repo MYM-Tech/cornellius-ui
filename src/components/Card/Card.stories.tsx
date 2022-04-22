16c03b33480e5ecffd6343767cc6093d249b7ef4
Bastien WEBER <weber.bastien@gmail.com>
2022-04-22 15:29:00 +0200

    build: add linters and fix false react typings

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,35 +1,33 @@
-import {Card} from './Card'
+import { Meta, StoryFn } from '@storybook/vue3';
+import { Card, CardProps } from './Card';
 
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Card',
-  component: Card,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    onClick: {},
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
+    title: 'Example/Card',
+    component: Card,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+        onClick: {},
+        size: {
+            control: { type: 'select' },
+            options: ['small', 'medium', 'large'],
+        },
     },
-  },
+} as Meta;
+
+type CardStoryArgs = CardProps & {
+    content: string;
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
-const Template = (args: any) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { Card },
-  // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    return { args };
-  },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<Card v-bind="args" />',
+const Template: StoryFn<CardStoryArgs> = (args) => ({
+    components: { Card },
+    setup() {
+        return { args };
+    },
+    template: `<Card v-bind="args">${args.content}</Card>`,
 });
 
-export const Primary:any = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  slots: {default: "hola"},
+    primary: true,
+    content: 'This is a card',
 };
