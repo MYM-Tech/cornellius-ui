aaf700147d05048b45acec2c463bc167846fdad8
Bastien WEBER <weber.bastien@gmail.com>
2022-04-22 15:29:00 +0200

    build: add linters and fix false react typings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,56 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "browser": true,
-    "node": true,
-    "es2021": true
+  root: true,
+  env: {
+      node: true,
   },
-  "extends": [
-    "eslint:recommended", 
-    "plugin:vue/essential", 
-    "plugin:@typescript-eslint/recommended", 
-    "plugin:vue/vue3-recommended", 
-    "eslint:recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "parser": "@typescript-eslint/parser",
-    "sourceType": "module"
+  extends: [
+      'plugin:vue/vue3-essential',
+      '@vue/airbnb',
+      '@vue/typescript/recommended',
+      'plugin:storybook/recommended',
+      'prettier',
+  ],
+  parserOptions: {
+      ecmaVersion: 2020,
   },
-  "plugins": ["vue", "@typescript-eslint"],
-  "rules": {}
+  plugins: ['prettier'],
+  rules: {
+      'vue/require-default-prop': 'off',
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'prettier/prettier': 'warn',
+      'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+              js: 'never',
+              jsx: 'never',
+              ts: 'never',
+              tsx: 'never',
+              '': 'never',
+          },
+      ],
+      'import/no-extraneous-dependencies': [
+          'error',
+          {
+              devDependencies: ['**/*.stories.*', '**/.storybook/**/*.*'],
+              peerDependencies: true,
+          },
+      ],
+  },
+  overrides: [
+      {
+          files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+          env: {
+              jest: true,
+          },
+      },
+  ],
+  settings: {
+      'import/resolver': {
+          node: {
+              extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+          },
+      },
+  },
 };
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,20 @@
 import { FunctionalComponent } from 'vue';
-import CSS from './Card.module.scss'
-import classnames from 'classnames'
+import classnames from 'classnames';
+import CSS from './Card.module.scss';
+
 export interface CardProps {
-  primary: boolean;
-  borderRadius: boolean;
+    primary: boolean;
+    borderRadius: boolean;
 }
 
-export const Card: FunctionalComponent<CardProps> = ({primary, borderRadius}, {slots}) => {
-  
-  const className= `${CSS.card} ${primary? CSS.card__primary: ""} ${borderRadius? "": CSS.card__nonBoderRadius}`;
-  
-  return (
-    <div 
-      class={
-        classnames(
-          CSS.card, 
-          primary? CSS.card__primary: "", 
-          borderRadius? "": CSS.card__nonBoderRadius
-        )
-      }>
+export const Card: FunctionalComponent<CardProps> = ({ primary, borderRadius }, { slots }) => (
+    <div
+        class={classnames(
+            CSS.card,
+            primary ? CSS.card__primary : '',
+            borderRadius ? '' : CSS.card__nonBoderRadius
+        )}
+    >
         {slots.default && slots.default()}
     </div>
-  )
-}
+);
