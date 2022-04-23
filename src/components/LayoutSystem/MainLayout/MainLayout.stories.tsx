50826369589d7bf2282e1388750a35a5e6262791
MARIN <josermarinr18@gmail.com>
2022-04-23 17:36:29 +0200

    Build: Implementing layout system, the good type of build and fixing error on css inyected in js build

---
+++ src/components/LayoutSystem/MainLayout/MainLayout.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryFn } from '@storybook/vue3';
+import { MainLayout, MainLayoutProps } from './MainLayout';
+
+export default {
+  title: 'Layout System/MainLayout',
+  component: MainLayout,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+type MainStoryProps = MainLayoutProps & {
+  style: string;
+};
+
+const mainStory: StoryFn<MainStoryProps> = (args) => ({
+  components: { MainLayout },
+  setup() {
+    return { args };
+  },
+  template: `
+    <MainLayout v-bind="args" reverseSideBar="false">
+      <div style="background: #0AD561; height: 100%;">
+      </div>
+    </MainLayout>`,
+});
+
+export const Main = mainStory.bind({});
+
+Main.args = {
+  style: 'background: white; border:1px solid red',
+};
