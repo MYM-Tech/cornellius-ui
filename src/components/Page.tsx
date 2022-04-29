68bf6cfbc84ad4e8432c1971b262c85303f5e2cc
Bastien WEBER <weber.bastien@gmail.com>
2022-04-29 10:58:41 +0200

    feat: input text

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,13 +1,13 @@
-import HelloWorld from "./HelloWorld"
-import logo from "./../assets/logo.png"
-import { Card } from "./Card/Card"
+import HelloWorld from './HelloWorld';
+import logo from '../assets/logo.png';
+import { Card } from './Card/Card';
 
-export default () => {
-  return (
-    <div> 
+export default () => (
+    <div>
         <img alt="Vue logo" src={logo} />
         <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-        <Card borderRadius={false} primary={true}>YOLO</Card>
+        <Card borderRadius={false} primary={true}>
+            YOLO
+        </Card>
     </div>
-  )
-}
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import { Card } from "./components/Card/Card"
-import { Content } from "./components/LayoutSystem/Content/Content"
-import { MainLayout } from "./components/LayoutSystem/MainLayout/MainLayout"
-import { Sidebar } from "./components/LayoutSystem/Sidebar/Sidebar"
+import './global.scss';
 
+import { Card } from './components/Card/Card';
+import { Content } from './components/LayoutSystem/Content/Content';
+import { MainLayout } from './components/LayoutSystem/MainLayout/MainLayout';
+import { Sidebar } from './components/LayoutSystem/Sidebar/Sidebar';
 
-export {Card, Content, MainLayout, Sidebar}
+export { Card, Content, MainLayout, Sidebar };
