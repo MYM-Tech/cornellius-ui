abfe2f8c5e8521a07882f7ffb19dac0b71da38f7
MARIN <josermarinr18@gmail.com>
2022-05-02 10:23:59 +0200

    Build: Local CI, and adding test storybook on gitaction

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import { Card } from './components/Card/Card';
 import { Content } from './components/LayoutSystem/Content/Content';
 import { MainLayout } from './components/LayoutSystem/MainLayout/MainLayout';
 import { Sidebar } from './components/LayoutSystem/Sidebar/Sidebar';
+import CorInputText from './components/Inputs/CorInputText/CorInputText';
 
-export { Card, Content, MainLayout, Sidebar };
+export { Card, Content, MainLayout, Sidebar, CorInputText };
