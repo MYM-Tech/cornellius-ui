ea9bd3047c259ae33610dd6fe0c7c5460437f557
Bastien WEBER <weber.bastien@gmail.com>
2022-05-03 16:18:53 +0200

    feat: add component in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,6 @@ import { Content } from './components/LayoutSystem/Content/Content';
 import { MainLayout } from './components/LayoutSystem/MainLayout/MainLayout';
 import { Sidebar } from './components/LayoutSystem/Sidebar/Sidebar';
 import CorInputText from './components/Inputs/CorInputText/CorInputText';
+import CorInputNumber from './components/Inputs/CorInputNumber/CorInputNumber';
 
-export { Card, Content, MainLayout, Sidebar, CorInputText };
+export { Card, Content, MainLayout, Sidebar, CorInputText, CorInputNumber };
