4f42031c40a459aeba357c032bccdb04e3c9068c
Bastien WEBER <weber.bastien@gmail.com>
2022-05-16 16:25:36 +0200

    feat: add custom icon prop on checkbox

--- src/components/Inputs/CorInputCheckbox/CorInputCheckbox.types.ts
+++ src/components/Inputs/CorInputCheckbox/CorInputCheckbox.types.ts
@@ -1,8 +1,11 @@
+import { VNode } from 'vue';
+
 export interface CorInputCheckboxProps {
     id: string;
     label: string;
     checked?: boolean;
     disabled?: boolean;
     name: string;
+    customIcon?: VNode | VNode[];
     onChange?(e: Event): void;
 }
