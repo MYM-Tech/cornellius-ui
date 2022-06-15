40e0ab1383d9684077e63c2031d20647abf2ff80
MARIN <josermarinr18@gmail.com>
2022-06-15 14:37:14 +0200

    Build: workflow to github page and cromatic Refacto: linter in layout system  Changes to be committed: 	new file:   .github/workflows/chromatic-deploy.yml 	new file:   .github/workflows/storybook-gitpage.yml 	deleted:    .github/workflows/storybook.yml 	modified:   package.json 	modified:   src/components/LayoutSystem/Content/Content.stories.tsx 	modified:   src/components/LayoutSystem/Content/Content.tsx 	modified:   src/components/LayoutSystem/MainLayout/MainLayout.stories.tsx 	modified:   src/components/LayoutSystem/MainLayout/MainLayout.tsx 	modified:   src/components/LayoutSystem/Sidebar/Sidebar.stories.tsx 	modified:   src/components/LayoutSystem/Sidebar/Sidebar.tsx 	modified:   yarn.lock

--- src/components/LayoutSystem/Content/Content.stories.tsx
+++ src/components/LayoutSystem/Content/Content.stories.tsx
@@ -4,25 +4,25 @@ import { Sidebar } from '../Sidebar/Sidebar';
 import { Content, ContentProps } from './Content';
 
 export default {
-  title: 'Layout System/Content',
-  component: Content,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Layout System/Content',
+    component: Content,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as Meta;
 
 type ContentStoryArgs = ContentProps & {
-  style: string;
-  reverse: boolean;
-  backgroundColor: string
+    style: string;
+    reverse: boolean;
+    backgroundColor: string;
 };
 
 const contentStorySidebar: StoryFn<ContentStoryArgs> = (args) => ({
-  components: { MainLayout, Sidebar, Content },
-  setup() {
-    return { args };
-  },
-  template: `
+    components: { MainLayout, Sidebar, Content },
+    setup() {
+        return { args };
+    },
+    template: `
     <MainLayout v-bind:reverseSidebar="args.reverse" v-bind:style="args.style">
       <Sidebar>
         <div style="background: #e0efe7; height: 100%;"></div>
@@ -36,16 +36,16 @@ const contentStorySidebar: StoryFn<ContentStoryArgs> = (args) => ({
 export const ContentWithSidebar = contentStorySidebar.bind({});
 
 ContentWithSidebar.args = {
-  reverse: false,
-  style: 'background: white; border:1px solid red',
+    reverse: false,
+    style: 'background: white; border:1px solid red',
 };
 
 const contentStory: StoryFn<ContentStoryArgs> = (args) => ({
-  components: { MainLayout, Content },
-  setup() {
-    return { args };
-  },
-  template: `
+    components: { MainLayout, Content },
+    setup() {
+        return { args };
+    },
+    template: `
     <MainLayout v-bind:style="args.style" v-bind:reverseSidebar="args.reverse">
       <Content v-bind:fullSize="args.fullSize">
         <div style='background: args.backgroundColor; height: 100%;'></div>
@@ -56,8 +56,8 @@ const contentStory: StoryFn<ContentStoryArgs> = (args) => ({
 export const ContentWithoutSidebar = contentStory.bind({});
 
 ContentWithoutSidebar.args = {
-  fullSize: true,
-  reverse: false,
-  style: 'background: white; border:1px solid red',
-  backgroundColor: '#0AD561',
+    fullSize: true,
+    reverse: false,
+    style: 'background: white; border:1px solid red',
+    backgroundColor: '#0AD561',
 };
--- src/components/LayoutSystem/Sidebar/Sidebar.tsx
+++ src/components/LayoutSystem/Sidebar/Sidebar.tsx
@@ -3,11 +3,10 @@ import { FunctionalComponent } from 'vue';
 import STYLE from './Sidebar.module.scss';
 
 export interface SidebarProps {
-  className?: string;
+    className?: string;
 }
 
-export const Sidebar: FunctionalComponent<SidebarProps> = ({className}: SidebarProps, { slots }) => {
-  return <div class={classNames(STYLE.sidebar, className)}>
-            {slots.default && slots.default()}
-        </div>;
-};
+export const Sidebar: FunctionalComponent<SidebarProps> = (
+    { className }: SidebarProps,
+    { slots }
+) => <div class={classNames(STYLE.sidebar, className)}>{slots.default && slots.default()}</div>;
