3d32babce49f2c1e2c2fffd567e3edf83ff27da0
MARIN <josermarinr18@gmail.com>
2022-06-17 21:05:48 +0200

    Chore: style improvement

--- src/components/Inputs/CorInputSwitch/CorInputSwitch.stories.tsx
+++ src/components/Inputs/CorInputSwitch/CorInputSwitch.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryFn } from '@storybook/vue3';
 import CorInputSwitch from './CorInputSwitch';
+import { CorInputSwitchPorps } from './CorInputSwitch.types';
 
 export default {
     title: 'Inputs/Input Switch',
@@ -7,11 +8,10 @@ export default {
     argTypes: {
         label: { type: 'string' },
         disabled: { type: 'boolean' },
-        radioPosition: { control: 'select', options: ['left', 'right'] },
     },
 } as Meta;
 
-const Template = (args: any) => ({
+const Template: StoryFn<CorInputSwitchPorps> = (args) => ({
     components: { CorInputSwitch },
     setup() {
         return { args };
@@ -19,10 +19,37 @@ const Template = (args: any) => ({
     template: `<CorInputSwitch v-bind="args"/>`,
 });
 
-export const Default: any = Template.bind({});
+export const Default = Template.bind({});
 Default.args = {
     label: 'Label',
     disabled: false,
-    value: true,
+    checked: true,
     onChange: (e) => console.log(e),
 };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    label: 'Label',
+    disabled: true,
+    checked: true,
+    onChange: (e) => console.log(e),
+};
+
+
+
+const MultiTemplate: StoryFn = (args) => ({
+    components: { CorInputSwitch },
+    setup() {
+        return { args };
+    },
+    template: `
+        <div>
+            <CorInputSwitch  label="First input" checked="true"/>
+            <CorInputSwitch  label="Second input" />
+            <CorInputSwitch  label="Third input" checked="true" />
+            <CorInputSwitch  label="Fourth input" />
+        </div>
+    `,
+});
+
+export const MultipleInput = MultiTemplate.bind({});
