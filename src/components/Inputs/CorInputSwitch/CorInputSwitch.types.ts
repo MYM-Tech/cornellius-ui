bba41afd5798f5bc8924c8fb1c99d8da968ab75e
MARIN <josermarinr18@gmail.com>
2022-06-24 17:01:26 +0200

    Chore: New props for input switch Docs: Mdx file for input switch  Changes to be committed: 	modified:   src/components/Inputs/CorInputSwitch/CorInpurSwitch.module.scss 	new file:   src/components/Inputs/CorInputSwitch/CorInputSwitch.stories.mdx 	modified:   src/components/Inputs/CorInputSwitch/CorInputSwitch.stories.tsx 	modified:   src/components/Inputs/CorInputSwitch/CorInputSwitch.tsx 	modified:   src/components/Inputs/CorInputSwitch/CorInputSwitch.types.ts

--- src/components/Inputs/CorInputSwitch/CorInputSwitch.types.ts
+++ src/components/Inputs/CorInputSwitch/CorInputSwitch.types.ts
@@ -4,4 +4,8 @@ export interface CorInputSwitchPorps {
     onChange?: (event: Event) => void;
     checked?: boolean;
     disabled?: boolean;
+    inline?: boolean;
+    labelPosition: 'left' | 'right';
+    className?: string | string[];
+    classSwitch?: string | string[];
 }
